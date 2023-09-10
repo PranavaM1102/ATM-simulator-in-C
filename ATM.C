f5f4bdb9ffc589c1a3e15556d01471f353ebb2bd
PRANAVA M <43328646+PranavaM1102@users.noreply.github.com>
2023-09-10 18:26:02 +0530

    Update ATM.C

--- ATM.C
+++ ATM.C
@@ -1,109 +1,109 @@
-
 #include <stdio.h>
-int main()
-{
 
-	FILE *fp0;
-	fp0=fopen("receipt.txt","w");
-	fprintf(fp0,"---------------------Welcome to ABC BANK ATM Service-------------------\n");
-	printf("---------------------Welcome to ABC BANK ATM Service-------------------\n");
-	unsigned long 	balance, deposit, withdraw;
-	int choice,pin,pin1,k=0;
-	char transaction;
-    FILE *fp;
-    fp=fopen("pin.txt","r");
-	fscanf(fp,"%d",&pin);
-	fclose(fp);
-	while (pin1!=pin)
-	{
-		printf("ENTER YOUR ATM PIN:");
-		scanf("%d", &pin1);
-		if (pin1 != pin)
-		printf("PLEASE ENTER VALID PIN\n");
-	}
-	do
-	{
-		
-		printf("---------------------Welcome to ABC BANK ATM Service-------------------\n");
-		printf("1. Check Balance");
-		printf("                                           2. Withdraw Cash\n");
-		printf("3. Deposit Cash");
-		printf("                                           4. Change The Pin\n");
-		printf("                       5. Quit\n");
-		printf("_______________________________________________________________\n\n");
-		printf("Enter your choice: ");
-		scanf("%d", &choice);
-		switch (choice)
-		{
-		case 1:
-			fp=fopen("balance.txt","r");
-			fscanf(fp,"%d",&balance);
-			fclose(fp);
-			printf("\n YOUR BALANCE IN Rs : %lu ", balance);
-			break;
-		case 2:
-			fp=fopen("balance.txt","r");
-			fscanf(fp,"%d",&balance);
-			fclose(fp);
-			printf("\n ENTER THE AMOUNT TO WITHDRAW: ");
-			scanf("%lu", &withdraw);
-			if (withdraw % 100 != 0)
-			{
-				printf("\n PLEASE ENTER THE AMOUNT IN MULTIPLES OF 100");
-			}
-			else if (withdraw >(balance - 500))
-			{
-				printf("\n INSUFFICENT BALANCE");
-			}
-			else
-			{
-				fp=fopen("balance.txt","w");
-				fprintf(fp0,"\n YOUR CURRENT BALANCE IS %lu", balance);
-				balance = balance - withdraw;
-				fprintf(fp0,"\n%lu WAS DEBITED FROM YOUR ACCOUNT", withdraw);
-				fprintf(fp,"%d",balance);
-				fclose(fp);
-				printf("\n\n PLEASE COLLECT CASH");
-				printf("\n YOUR CURRENT BALANCE IS %lu", balance);
-				fprintf(fp0,"\n YOUR CURRENT BALANCE IS %lu", balance);
-			}
-			break;
-		case 3:
-			fp=fopen("balance.txt","r");
-			fscanf(fp,"%d",&balance);
-			fclose(fp);
-			fprintf(fp0,"\n YOUR CURRENT BALANCE IS %lu", balance);
-			printf("\n ENTER THE AMOUNT TO DEPOSIT");
-			scanf("%lu", &deposit);
-                        balance = balance + deposit;
-			fp=fopen("balance.txt","w");
-			fprintf(fp0,"\n%lu WAS CREDITED FROM YOUR ACCOUNT", deposit);
-			fprintf(fp,"%d",balance);
-			fprintf(fp0,"\n YOUR CURRENT BALANCE IS %lu", balance);
-			fclose(fp);
-			
-			printf("YOUR BALANCE IS %lu", balance);
-			break;
-		case 5:
-			printf("\n THANK U USING ATM");
-			break;
-		case 4:
-			fp=fopen("pin.txt","w");
-			printf("ENTER THE NEW PIN\n");
-			scanf("%d",&pin);
-			fprintf(fp,"%d",pin);
-			fclose(fp);
-			break;
-		default:
-			printf("\n INVALID CHOICE");
-		}
-	printf("\n\n\n DO U WISH TO HAVE ANOTHER TRANSCATION?(y/n): \n");
-	fflush(stdin);
-	scanf("%c", &transaction);
-	if (transaction == 'n'|| transaction == 'N')
-        k = 1;
-	} while (!k);
-	fprintf(fp0,"\n\n THANKS FOR USING ABC BANK SERVICE\nWEAR MASK AND SANITIZE UR HANDS\n DO VISIT AGAIN\n THANKS FOR USING ABC BANK SERVICE");
-	fclose(fp0);
-	printf("\n\n THANKS FOR USING ABC BANK SERVICE\nWEAR MASK AND SANITIZE UR HANDS\n DO VISIT AGAIN\n THANKS FOR USING ABC BANK SERVICE");
+void displayMenu() {
+    printf("---------------------Welcome to ABC BANK ATM Service-------------------\n");
+    printf("1. Check Balance\n");
+    printf("2. Withdraw Cash\n");
+    printf("3. Deposit Cash\n");
+    printf("4. Change The Pin\n");
+    printf("5. Quit\n");
+    printf("_______________________________________________________________\n\n");
+    printf("Enter your choice: ");
+}
+
+int main() {
+    FILE *fp0, *fp;
+    int balance, deposit, withdraw;
+    int choice, pin, pin1, k = 0;
+    char transaction;
+    
+    fp = fopen("pin.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening pin.txt\n");
+        return 1;
+    }
+    fscanf(fp, "%d", &pin);
+    fclose(fp);
+
+    do {
+        printf("ENTER YOUR ATM PIN: ");
+        scanf("%d", &pin1);
+        if (pin1 != pin) {
+            printf("PLEASE ENTER VALID PIN\n");
+        }
+    } while (pin1 != pin);
+
+    fp = fopen("balance.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening balance.txt\n");
+        return 1;
+    }
+    fscanf(fp, "%d", &balance);
+    fclose(fp);
+
+    fp0 = fopen("receipt.txt", "w");
+    if (fp0 == NULL) {
+        printf("Error opening receipt.txt\n");
+        return 1;
+    }
+
+    do {
+        displayMenu();
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("\nYOUR BALANCE IN Rs : %d\n", balance);
+                break;
+            case 2:
+                printf("\nENTER THE AMOUNT TO WITHDRAW: ");
+                scanf("%d", &withdraw);
+                if (withdraw % 100 != 0) {
+                    printf("\nPLEASE ENTER THE AMOUNT IN MULTIPLES OF 100\n");
+                } else if (withdraw > (balance - 500)) {
+                    printf("\nINSUFFICIENT BALANCE\n");
+                } else {
+                    balance -= withdraw;
+                    printf("\nPLEASE COLLECT CASH\n");
+                    printf("YOUR CURRENT BALANCE IS %d\n", balance);
+                    fprintf(fp0, "\nYOUR CURRENT BALANCE IS %d", balance);
+                }
+                break;
+            case 3:
+                printf("\nENTER THE AMOUNT TO DEPOSIT: ");
+                scanf("%d", &deposit);
+                balance += deposit;
+                printf("YOUR BALANCE IS %d\n", balance);
+                fprintf(fp0, "\nYOUR CURRENT BALANCE IS %d", balance);
+                break;
+            case 4:
+                fp = fopen("pin.txt", "w");
+                printf("ENTER THE NEW PIN: ");
+                scanf("%d", &pin);
+                fprintf(fp, "%d", pin);
+                fclose(fp);
+                break;
+            case 5:
+                printf("\nTHANK YOU FOR USING ABC BANK ATM\n");
+                break;
+            default:
+                printf("\nINVALID CHOICE\n");
+                break;
+        }
+
+        printf("\nDO YOU WISH TO HAVE ANOTHER TRANSACTION? (y/n): ");
+        fflush(stdin);
+        scanf(" %c", &transaction);
+
+        if (transaction == 'n' || transaction == 'N') {
+            k = 1;
+        }
+    } while (!k);
+
+    fprintf(fp0, "\n\nTHANK YOU FOR USING ABC BANK SERVICE\nWEAR MASK AND SANITIZE YOUR HANDS\nDO VISIT AGAIN\nTHANK YOU FOR USING ABC BANK SERVICE\n");
+    fclose(fp0);
+
+    printf("\nTHANK YOU FOR USING ABC BANK SERVICE\nWEAR MASK AND SANITIZE YOUR HANDS\nDO VISIT AGAIN\nTHANK YOU FOR USING ABC BANK SERVICE\n");
+
+    return 0;
 }
